4bd0e94aa52f3a9ece7bd53721ca2d1c51e90440
kukunju <132256077+kukunju@users.noreply.github.com>
2023-05-18 13:11:20 +0000

    [tyuudann]5-18

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/show'
+  get 'users/index'
+  get 'books/edit'
+  get 'books/show'
+  get 'books/index'
+  get 'homes/home'
+  get 'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
