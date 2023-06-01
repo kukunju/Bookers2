d6559baae60432b1d93ed7432eb1998751b2733e
kukunju <132256077+kukunju@users.noreply.github.com>
2023-06-01 10:54:05 +0000

    [Update]フォロー機能の修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,22 @@
 class RelationshipsController < ApplicationController
 
-
+#連打されたとき重複しないための条件分岐！！！
   def create
+    followed=User.find(params[:user_id])
+    unless followed.followed_by?(current_user)
     follow = current_user.follower_relationships.build(followed_id: params[:user_id])
     follow.save
+    end
+
     redirect_to request.referer
   end
 
   def destroy
+    followed=User.find(params[:user_id])
+    if followed.followed_by?(current_user)
     follow = current_user.follower_relationships.find_by(followed_id: params[:user_id])
     follow.destroy
+  end
     redirect_to request.referer
   end
 
