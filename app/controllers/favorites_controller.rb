07c92ebb9f4fc1aab2a0b473e0db94435a1426f0
kukunju <132256077+kukunju@users.noreply.github.com>
2023-06-01 12:46:18 +0000

    [Update]いいね機能修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,15 +2,19 @@ class FavoritesController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
+    unless book.favorited_by?(current_user)
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
+  end
     redirect_to request.referer
   end
 
   def destroy
     book = Book.find(params[:book_id])
+    if book.favorited_by?(current_user)
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
+  end
     redirect_to request.referer
   end
 
