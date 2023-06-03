2e90dffa1989e28b6a6f15ab4d6d1ce5a007fcc5
kukunju <132256077+kukunju@users.noreply.github.com>
2023-06-03 04:55:23 +0000

    [Add]ゲストログイン機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_guest_user, only: [:edit]
+
   def edit
     @user = User.find(params[:id])
     unless @user.id == current_user.id
@@ -51,6 +53,14 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user), notice: 'ゲストユーザーはプロフィール編集画面へ移行できません。'
+
+    end
   end
 
 end
