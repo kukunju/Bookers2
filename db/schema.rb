437b90a0f8517711d24a8c5269dca82010ab0df7
kukunju <132256077+kukunju@users.noreply.github.com>
2023-06-05 09:31:28 +0000

    [start]応用課題7d

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_28_111555) do
+ActiveRecord::Schema.define(version: 2023_06_05_092944) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2023_05_28_111555) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "star"
   end
 
   create_table "comments", force: :cascade do |t|
