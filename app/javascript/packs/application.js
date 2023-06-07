415b12e2fea3a0ae5a12ae67d5d35a6713c47a5c
kukunju <132256077+kukunju@users.noreply.github.com>
2023-06-07 13:40:48 +0000

    [Add]Raty

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,14 @@ import "../stylesheets/application"
 
 import '@fortawesome/fontawesome-free/js/all'
 
+import Raty from "raty.js"
+window.raty = function(elem,opt) 
+{
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
